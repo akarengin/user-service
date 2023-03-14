6d23faa16b912a8623c8e7f7f088f86cb5aba588
akarengin <engin.akar1@gmail.com>
2023-03-14 14:24:33 +0300

    first commit

---
+++ src/main/java/com/example/user/service/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.user.service.repository;
+
+import com.example.user.service.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUserId(Long userId);
+}
